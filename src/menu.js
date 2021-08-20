b606d59a65a23f3097c8da68658f48be098ebc52
SetRedEyes <denidraven@gmail.com>
2021-08-20 20:43:18 +0300

    Implemented context menu

--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,22 @@
-import {Menu} from './core/menu'
-
+import { Menu } from './core/menu'
 export class ContextMenu extends Menu {
+  constructor(selector) {
+    super(selector)
+    document.body.addEventListener('contextmenu', (e) => {
+      e.preventDefault()
 
+      this.open(e)
+    })
+  }
+  open(e) {
+    this.el.classList.add('open')
+    const { clientX: mouseX, clientY: mouseY } = e
+    this.el.style.top = `${mouseY}px`
+    this.el.style.left = `${mouseX}px`
+  }
+
+  close() {
+    this.el.classList.remove('open')
+  }
+  add() {}
 }
